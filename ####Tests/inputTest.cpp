2d4f886eafd24b7be177bec89a1089bb0cbd4772
George Kimaru <george.kimaru98@gmail.com>
2021-09-01 13:08:37 +0300

    initial commit

---
+++ ####Tests/inputTest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout << "Enter a test number: \n";
+    cin >> number;
+    cout << number << endl;
+    // if (number > 5){
+    //     cout << "Greater than 5";
+    // }
+    // else if (number < 5){
+    //     cout << "Less than 5";
+    // }
+    // else {
+    //     cout << "Enter a valid number";
+    // }
+}
